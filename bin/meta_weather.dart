66c9f44ae96598f33efc2c03e96c1f64c3dc06dd
Vasile <allfortrashmail@gmail.com>
2023-01-02 17:02:42 +0200

    prepare main

--- bin/meta_weather.dart
+++ bin/meta_weather.dart
@@ -1,10 +1,13 @@
+import 'package:meta_weather/meta_weather.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async{
   if (arguments.isEmpty) {
     print('Syntax: dart bin/main.dart <location>');
     return;
   }
 
-  final location = arguments.first;
-  print(location);
+  final city = arguments.first;
+  final weatherApiClient = WeatherApiClient();
+  final weather = await weatherApiClient.fetchWeatherByCity(city);
+  print(weather);
 }
