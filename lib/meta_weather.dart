d882878508169e3b8e5f27a6b00c42ecbbcb7bf9
Vasile <allfortrashmail@gmail.com>
2023-01-03 10:33:31 +0200

    format code

--- lib/meta_weather.dart
+++ lib/meta_weather.dart
@@ -1,15 +1,19 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+
 import 'weather.dart';
 
 class WeatherApiException implements Exception {
   const WeatherApiException(this.message);
+
   final String message;
 }
 
 class WeatherApiClient {
   static const _baseUrl = 'api.weatherapi.com';
   static const _baseUrlVersion = '/v1/current.json';
+
   static Map<String, String> _locationOptions(String location) =>
       {'key': 'YOUR_API_KEY', 'q': location, 'aqi': 'yes'};
 
@@ -25,7 +29,7 @@ class WeatherApiClient {
     final Map<String, dynamic> jsonResponse =
         Map.castFrom(jsonDecode(response.body));
 
-    if(jsonResponse.isEmpty) {
+    if (jsonResponse.isEmpty) {
       throw WeatherApiException('Weather data for $city not found');
     }
 
